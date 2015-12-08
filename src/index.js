6c08e3c1d0913cdec93f319b6cda0965d388f314
egoist <0x142857@gmail.com>
2015-12-08 09:49:06 +0800

    add postcss opts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import postcss from 'postcss';
 import styleInject from 'style-inject';
+import path from 'path';
+
+function pathJoin (file) {
+  return path.join(process.cwd(), file);
+}
 
 export default function (options = {}) {
   return {
@@ -10,7 +15,15 @@ export default function (options = {}) {
       if (id.slice( -4 ) !== '.css') {
         return null;
       }
-      code = postcss(options.plugins || []).process(code).css;
+      const opts = {
+        from: options.from ? pathJoin(options.from) : id,
+        to: options.to ? pathJoin(options.to) : id,
+        map: {
+          inline:     false,
+          annotation: false
+        }
+      };
+      code = postcss(options.plugins || []).process(code, opts).css;
       code = `export default styleInject(${JSON.stringify(code)});`
       return {
         code,
