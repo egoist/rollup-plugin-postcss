d09b93fea58b8756ea656e4feba5b9cc4dd1870f
Lajos György Mészáros <meszaros-lajos-gyorgy@users.noreply.github.com>
2017-07-25 23:25:33 +0200

    Added watch functionality for imported files (#31)
    * Added helper functions, did a bit of refactoring
    
    * Separated out the internals of transform; Did a bit more refactoring
    
    * Refactored _transform() to reduce the number of returns needed
    
    * Moved contents of intro to a separate function; Rewrote Object.keys().forEach() methods to Object.entries().forEach() for readability
    
    * Added outlines of Watcher class
    
    * Added file watcher logic
    
    * Added rebuilding code, tweaked watcher to only add or remove watchers, when necessary.
    
    * Fixed errors
    
    * Fixed more issues based on the test script
    
    * Tweaked logging of files, which are being watched
    
    * Tweaked watcher logging
    
    * Fixing whitespace according to lint error
    
    * Transform named exported classNames to accepted js var names and other fixes
    
    - fixed wrong sourcemap reference with manual destination (#32, #33)
    - added getExportNamed param to allow/ named exports when used with css
    modules (#27, #30)
    - when using name exports, all the classNames that contains dashes, or
    they are js protected names, will be transformed to usable js names.
    (#27, #28)
    - name transformations are logged out for usage information.
    - added new unit tests and updated existing tests to cover more
    scenarios
    
    * -> v0.5.4
    
    * Added helper functions, did a bit of refactoring
    
    * Separated out the internals of transform; Did a bit more refactoring
    
    * Refactored _transform() to reduce the number of returns needed
    
    * Moved contents of intro to a separate function; Rewrote Object.keys().forEach() methods to Object.entries().forEach() for readability
    
    * Added outlines of Watcher class
    
    * Added file watcher logic
    
    * Added rebuilding code, tweaked watcher to only add or remove watchers, when necessary.
    
    * Fixed errors
    
    * Fixed more issues based on the test script
    
    * Tweaked logging of files, which are being watched
    
    * Tweaked watcher logging
    
    * Fixing whitespace according to lint error
    
    * Fixed issue spotted by the linter
    
    * Another set of linter issues
    
    * Yet another set of changes required by the linter
    
    * Apparently I didn't get the previous requests of the linter fully, so here, have more commits

---
+++ src/helpers.js
@@ -0,0 +1,21 @@
+function isFunction(f) {
+  return typeof f === 'function'
+}
+
+function isString(s) {
+  return typeof s === 'string'
+}
+
+function dummyPreprocessor(code) {
+  return Promise.resolve({ code })
+}
+
+function clone(data) {
+  return typeof data === 'object' ? JSON.parse(JSON.stringify(data)) : data
+}
+
+function difference(a, b) {
+  return a.filter(ax => !b.includes(ax))
+}
+
+export { isFunction, isString, dummyPreprocessor, clone, difference }
