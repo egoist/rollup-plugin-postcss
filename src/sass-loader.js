88e4e85846eebc0dee44acdc27dfc87e228a1414
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-04-04 17:01:12 -0500

    fix: handle importing Sass partials in node_modules

--- src/sass-loader.js
+++ src/sass-loader.js
@@ -5,6 +5,11 @@ import importCwd from 'import-cwd'
 
 const moduleRe = /^~([a-z0-9]|@).+/i
 
+const getUrlOfPartial = url => {
+  const parsedUrl = path.parse(url)
+  return `${parsedUrl.dir}${path.sep}_${parsedUrl.base}`
+}
+
 export default {
   name: 'sass',
   test: /\.s[ac]ss$/,
@@ -19,18 +24,28 @@ export default {
       importer: [(url, importer, done) => {
         if (!moduleRe.test(url)) return done({ file: url })
 
-        resolve(url.slice(1), {
+        const moduleUrl = url.slice(1)
+        const partialUrl = getUrlOfPartial(moduleUrl)
+
+        const options = {
           basedir: path.dirname(importer),
           extensions: ['.scss', '.sass', '.css']
-        }, (err, id) => {
-          if (err) {
-            return Promise.reject(err)
-          }
+        }
+        const finishImport = id => {
           done({
             // Do not add `.css` extension in order to inline the file
             file: id.endsWith('.css') ? id.replace(/\.css$/, '') : id
           })
-        })
+
+          return id
+        }
+
+        // Give precedence to importing a partial
+        pify(resolve)(partialUrl, options)
+          .then(finishImport)
+          .catch(() => {
+            return pify(resolve)(moduleUrl, options).then(finishImport)
+          })
       }].concat(this.options.importer || [])
     })
 
