2ce93bad4f162e3fb53ea80779aba11780797367
EGOIST <0x142857@gmail.com>
2018-04-17 22:03:22 +0800

    tweaks

--- src/sass-loader.js
+++ src/sass-loader.js
@@ -10,6 +10,8 @@ const getUrlOfPartial = url => {
   return `${parsedUrl.dir}${path.sep}_${parsedUrl.base}`
 }
 
+const resolvePromise = pify(resolve)
+
 export default {
   name: 'sass',
   test: /\.s[ac]ss$/,
@@ -21,32 +23,43 @@ export default {
       data: code,
       indentedSyntax: /\.sass$/.test(this.id),
       sourceMap: this.sourceMap,
-      importer: [(url, importer, done) => {
-        if (!moduleRe.test(url)) return done({ file: url })
+      importer: [
+        (url, importer, done) => {
+          if (!moduleRe.test(url)) return done({ file: url })
 
-        const moduleUrl = url.slice(1)
-        const partialUrl = getUrlOfPartial(moduleUrl)
+          const moduleUrl = url.slice(1)
+          const partialUrl = getUrlOfPartial(moduleUrl)
 
-        const options = {
-          basedir: path.dirname(importer),
-          extensions: ['.scss', '.sass', '.css']
+          const options = {
+            basedir: path.dirname(importer),
+            extensions: ['.scss', '.sass', '.css']
+          }
+          const finishImport = id => {
+            done({
+              // Do not add `.css` extension in order to inline the file
+              file: id.endsWith('.css') ? id.replace(/\.css$/, '') : id
+            })
+          }
+
+          const next = () => {
+            // Catch all resolving errors, return the original file and pass responsibility back to other custom importers
+            done({ file: url })
+          }
+
+          // Give precedence to importing a partial
+          resolvePromise(partialUrl, options)
+            .then(finishImport)
+            .catch(err => {
+              if (err.code === 'MODULE_NOT_FOUND' || err.code === 'ENOENT') {
+                resolvePromise(moduleUrl, options)
+                  .then(finishImport)
+                  .catch(next)
+              } else {
+                next()
+              }
+            })
         }
-        const finishImport = id => {
-          done({
-            // Do not add `.css` extension in order to inline the file
-            file: id.endsWith('.css') ? id.replace(/\.css$/, '') : id
-          })
-
-          return id
-        }
-
-        // Give precedence to importing a partial
-        pify(resolve)(partialUrl, options)
-          .then(finishImport)
-          .catch(() => {
-            return pify(resolve)(moduleUrl, options).then(finishImport)
-          })
-      }].concat(this.options.importer || [])
+      ].concat(this.options.importer || [])
     })
 
     return {
