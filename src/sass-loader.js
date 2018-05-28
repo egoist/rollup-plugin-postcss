bf131cffe59ee362f5b2f87eb5cedfb56acbbbd5
Daniel Ström <D@nielstrom.se>
2018-05-28 16:35:13 +0200

    Limit threads when transpiling with node-sass (#105)
    This PR introduces a worker queue which limits the number of concurrent threads similar to how webpack [sass-loader](https://github.com/webpack-contrib/sass-loader/blob/2529c0716b1bca321c22d16636b1385682b1c730/lib/loader.js#L14) does. The changes are a workaround for the issues in node-sass discussed in https://github.com/egoist/rollup-plugin-postcss/issues/104.

--- src/sass-loader.js
+++ src/sass-loader.js
@@ -2,6 +2,12 @@ import path from 'path'
 import pify from 'pify'
 import resolve from 'resolve'
 import importCwd from 'import-cwd'
+import PQueue from 'p-queue'
+
+// This queue makes sure node-sass leaves one thread available for executing fs tasks
+// See: https://github.com/sass/node-sass/issues/857
+const threadPoolSize = process.env.UV_THREADPOOL_SIZE || 4
+const workQueue = new PQueue({ concurrency: threadPoolSize - 1 })
 
 const moduleRe = /^~([a-z0-9]|@).+/i
 
@@ -17,54 +23,59 @@ export default {
   test: /\.s[ac]ss$/,
   async process({ code }) {
     const sass = importCwd('node-sass')
-    const res = await pify(sass.render.bind(sass))({
-      ...this.options,
-      file: this.id,
-      data: code,
-      indentedSyntax: /\.sass$/.test(this.id),
-      sourceMap: this.sourceMap,
-      importer: [
-        (url, importer, done) => {
-          if (!moduleRe.test(url)) return done({ file: url })
 
-          const moduleUrl = url.slice(1)
-          const partialUrl = getUrlOfPartial(moduleUrl)
+    return new Promise((resolve, reject) => {
+      workQueue.add(() =>
+        pify(sass.render.bind(sass))({
+          ...this.options,
+          file: this.id,
+          data: code,
+          indentedSyntax: /\.sass$/.test(this.id),
+          sourceMap: this.sourceMap,
+          importer: [
+            (url, importer, done) => {
+              if (!moduleRe.test(url)) return done({ file: url })
 
-          const options = {
-            basedir: path.dirname(importer),
-            extensions: ['.scss', '.sass', '.css']
-          }
-          const finishImport = id => {
-            done({
-              // Do not add `.css` extension in order to inline the file
-              file: id.endsWith('.css') ? id.replace(/\.css$/, '') : id
-            })
-          }
+              const moduleUrl = url.slice(1)
+              const partialUrl = getUrlOfPartial(moduleUrl)
 
-          const next = () => {
-            // Catch all resolving errors, return the original file and pass responsibility back to other custom importers
-            done({ file: url })
-          }
-
-          // Give precedence to importing a partial
-          resolvePromise(partialUrl, options)
-            .then(finishImport)
-            .catch(err => {
-              if (err.code === 'MODULE_NOT_FOUND' || err.code === 'ENOENT') {
-                resolvePromise(moduleUrl, options)
-                  .then(finishImport)
-                  .catch(next)
-              } else {
-                next()
+              const options = {
+                basedir: path.dirname(importer),
+                extensions: ['.scss', '.sass', '.css']
               }
-            })
-        }
-      ].concat(this.options.importer || [])
-    })
+              const finishImport = id => {
+                done({
+                  // Do not add `.css` extension in order to inline the file
+                  file: id.endsWith('.css') ? id.replace(/\.css$/, '') : id
+                })
+              }
 
-    return {
-      code: res.css.toString(),
-      map: res.map && res.map.toString()
-    }
+              const next = () => {
+                // Catch all resolving errors, return the original file and pass responsibility back to other custom importers
+                done({ file: url })
+              }
+
+              // Give precedence to importing a partial
+              resolvePromise(partialUrl, options)
+                .then(finishImport)
+                .catch(err => {
+                  if (err.code === 'MODULE_NOT_FOUND' || err.code === 'ENOENT') {
+                    resolvePromise(moduleUrl, options)
+                      .then(finishImport)
+                      .catch(next)
+                  } else {
+                    next()
+                  }
+                })
+            }
+          ].concat(this.options.importer || [])
+        }).then(res =>
+          resolve({
+            code: res.css.toString(),
+            map: res.map && res.map.toString()
+          })
+        ).catch(reject)
+      )
+    })
   }
 }
