65e1c14d546cd326fab703bd0fa3900177ac877c
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2018-10-22 12:38:09 +0300

    Add test for watching an indirectly imported .styl file

--- test/watch.test.js
+++ test/watch.test.js
@@ -78,6 +78,10 @@ function expectEvents(watcher, events, timeout) {
   })
 }
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 test('rollup.watch() watches directly imported .styl files', async () => {
   function localfile(filename) {
     return pathUnderTestdir('test1', filename)
@@ -115,6 +119,48 @@ test('rollup.watch() watches directly imported .styl files', async () => {
   await expectEvents(watcher, ['START', 'BUNDLE_START', 'BUNDLE_END', 'END'], 2000)
 
   expect(read(localfile('output.css'))).toMatch(/#0f0/)
+})
 
-  await expectEvents(watcher, [], 2000)
+test('rollup.watch() watches indirectly imported .styl files', async () => {
+  function localfile(filename) {
+    return pathUnderTestdir('test2', filename)
+  }
+
+  write(localfile('input.js'), `import './a.styl'; console.log('hi')`)
+  write(localfile('a.styl'), `@import './b.styl'; body { color: #f0f }`)
+  write(localfile('b.styl'), 'h1 { color: #00f }')
+
+  const watchOptions = {
+    input: localfile('input.js'),
+    output: {
+      file: localfile('output.js'),
+      format: 'cjs',
+      sourcemap: 'inline'
+    },
+    plugins: [postcss({
+      extract: localfile('output.css'),
+      sourceMap: true
+    })]
+  }
+
+  const watcher = startWatcher(watchOptions)
+
+  await expectEvents(watcher, ['START', 'BUNDLE_START', 'BUNDLE_END', 'END'], 2000)
+
+  // It seems that it takes a while for rollup to actually start listening to file changes.
+  // Wait 2 seconds just in case.
+  await sleep(2000)
+
+  const resultingCss = read(localfile('output.css'))
+  expect(resultingCss).toMatch(/#f0f/)
+  expect(resultingCss).toMatch(/#00f/)
+
+  // Writing to an indirectly imported file should trigger a build...
+  write(localfile('b.styl'), 'h1 { color: #f00 }')
+  await expectEvents(watcher, ['START', 'BUNDLE_START', 'BUNDLE_END', 'END'], 2000)
+
+  // ...and the contents should be included in the output.
+  const resultingCss2 = read(localfile('output.css'))
+  expect(resultingCss2).toMatch(/#f0f/)
+  expect(resultingCss2).toMatch(/#f00/)
 })
