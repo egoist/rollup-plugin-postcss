acbb5f457c6d9309ce357e7dde729ff0ba8e912f
Julien Dargelos <julien.dargelos@me.com>
2019-09-13 17:00:47 +0200

    Use the emitFile method and update rollup version to at least 1.19.0

--- src/index.js
+++ src/index.js
@@ -180,19 +180,18 @@ export default (options = {}) => {
         }
       }
 
-      const codeFile = {
+      this.emitFile({
         fileName: codeFileName,
-        isAsset: true,
-        source: code
-      }
-      bundle[codeFile.fileName] = codeFile
+        source: code,
+        type: 'asset'
+      })
+
       if (map) {
-        const mapFile = {
+        this.emitFile({
           fileName: mapFileName,
-          isAsset: true,
+          type: 'asset',
           source: map
-        }
-        bundle[mapFile.fileName] = mapFile
+        })
       }
     }
   }
