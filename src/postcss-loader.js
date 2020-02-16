2d92e5784739065ce09d4ed5fbc1a84cf8af0c1a
Miguel <miguel@lottoland.com>
2020-02-16 18:47:09 +0100

    Added support for consuming stylesheet for later injection

--- src/postcss-loader.js
+++ src/postcss-loader.js
@@ -187,6 +187,7 @@ export default {
       output += `var css = ${JSON.stringify(res.css)};\nexport default ${
         supportModules ? JSON.stringify(modulesExported[this.id]) : 'css'
       };`
+      output += `\nexport const stylesheet=${JSON.stringify(res.css)};`;
     }
     if (!shouldExtract && shouldInject) {
       output += `\nimport styleInject from '${styleInjectPath}';\nstyleInject(css${
