c0b8428f8368aea362c8241cf32163cfad7b55c9
Miguel <miguel@lottoland.com>
2020-02-20 15:28:42 +0100

    Fixed Extra semicolon.

--- src/postcss-loader.js
+++ src/postcss-loader.js
@@ -186,7 +186,7 @@ export default {
     } else {
       output += `var css = ${JSON.stringify(res.css)};\nexport default ${
         supportModules ? JSON.stringify(modulesExported[this.id]) : 'css'
-      };`;
+      };`
       output += `\nexport const stylesheet=${JSON.stringify(res.css)};`;
     }
     if (!shouldExtract && shouldInject) {
