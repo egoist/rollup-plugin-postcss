37bb7a140ef6ff98221d55b8e29f50cfaa329a43
扩散性百万甜面包 <himself65@outlook.com>
2020-03-18 00:42:07 +0800

    feat: add the typescript declaration file (#241)
    Co-authored-by: Favna <support@favware.tech>

---
+++ types/index.d.ts
@@ -0,0 +1,44 @@
+import { Plugin } from 'rollup'
+import { CreateFilter } from 'rollup-pluginutils'
+
+type FunctionType<T = any, U = any> = (...args: T[]) => U;
+
+type onExtract = (asset: {
+  code: any
+  map: any
+  codeFileName: string
+  mapFileName: string
+}) => boolean
+
+export type PostCSSPluginConf = {
+  inject?: boolean | { [key: string]: any } | ((cssVariableName: string, id: string) => string)
+  extract?: boolean | string
+  onExtract?: onExtract
+  modules?: boolean | { [key: string]: any }
+  extensions?: string[]
+  plugins?: any[]
+  autoModules?: boolean
+  namedExports?: boolean | ((id: string) => boolean)
+  minimize?: boolean | any
+  parser?: string | FunctionType
+  stringifier?: string | FunctionType
+  syntax?: string | FunctionType
+  exec?: boolean
+  config?: boolean | {
+    path: string
+    ctx: any
+  };
+  to?: string
+  name?: any[] | any[][]
+  loaders?: any[]
+  onImport?: (id: string) => void
+  use?: string[] | { [key in 'sass' | 'stylus' | 'less']: any }
+  /**
+   * @default: false
+   **/
+  sourceMap?: boolean | 'inline'
+  include?: Parameters<CreateFilter>[0]
+  exclude?: Parameters<CreateFilter>[1]
+}
+
+export default function (options: PostCSSPluginConf): Plugin
